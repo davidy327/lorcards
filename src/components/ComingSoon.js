91379763be378fd66d8a999b9646c5c73c5ea3b1
David Yau <david.yau@mail.utoronto.ca>
2020-01-07 01:05:12 -0500

    added coming soon page for decks section

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ComingSoon() {
+  return (
+    <div className='container d-flex justify-content-center mt-5'>
+      <h1 className='text-white'>Coming Soon!</h1>
+    </div>
+  );
+}
+export default ComingSoon;
